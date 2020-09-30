ca7f1ca2002069a3a1789f82b5090425610fe8ea
p-toro <p.toro07943@gmail.com>
2020-09-30 17:53:33 +0900

    Remove unnecessary polyfill

--- script.ts
+++ script.ts
@@ -1,9 +1,6 @@
 import smoothscroll from 'smoothscroll-polyfill';
 smoothscroll.polyfill();
 
-import scrollSnapPolyfill from 'css-scroll-snap-polyfill';
-scrollSnapPolyfill();
-
 document.addEventListener('click', e => {
   const target = e.target;
   if(!target.classList.contains('js-smooth-scroll')) return;
